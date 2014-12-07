427b734dd47d1c3e063d11fbafff110bf989bfbd
NikolayIT <admin@nikolay.it>
2014-12-07 19:26:49 +0200

    Added new string extension method: ToUrl

---
+++ MissingFeatures.Tests/StringExtensions/ToUrlTests.cs
@@ -0,0 +1,118 @@
+﻿namespace MissingFeatures.Tests.StringExtensions
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ToUrlTests
+    {
+        [TestMethod]
+        public void ToUrlMethodShouldReturnProperCSharpText()
+        {
+            var original = "SomeUrlWithC#InIt";
+
+            var result = original.ToUrl();
+            var expected = "SomeUrlWithCSharpInIt";
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToUrlMethodShouldReturnProperCPlusPlusText()
+        {
+            var original = "SomeUrlWithC++InIt";
+
+            var result = original.ToUrl();
+            var expected = "SomeUrlWithCPlusPlusInIt";
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToUrlMethodShouldConvertUglySymbolsToDashInMiddle()
+        {
+            var original = "Some%Url&With!Ugly)Symbol";
+
+            var result = original.ToUrl();
+            var expected = "Some-Url-With-Ugly-Symbol";
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToUrlMethodShouldConvertUglySymbolsToDashInMiddleWithRepeatitions()
+        {
+            var original = "Some%$Url&!With!^^^Ugly**)Symbol";
+
+            var result = original.ToUrl();
+            var expected = "Some-Url-With-Ugly-Symbol";
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToUrlMethodShouldRemoveUglySymbolsInTheStartOfString()
+        {
+            var original = "###Some%$Url&!With!^^^Ugly**)Symbol";
+
+            var result = original.ToUrl();
+            var expected = "Some-Url-With-Ugly-Symbol";
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToUrlMethodShouldConvertUglySymbolsToDashInTheEndOfString()
+        {
+            var original = "Some%$Url&!With!^^^Ugly**)Symbol*&*";
+
+            var result = original.ToUrl();
+            var expected = "Some-Url-With-Ugly-Symbol";
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToUrlMethodShouldConvertSpacesToDash()
+        {
+            var original = "  Some  Url  With  Ugly  Symbol  ";
+
+            var result = original.ToUrl();
+            var expected = "Some-Url-With-Ugly-Symbol";
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToUrlMethodShouldConvertCSharpAndCPlusPlus()
+        {
+            var original = "  Some  C++UrlC++  With  UglyC#C#  Symbol  ";
+
+            var result = original.ToUrl();
+            var expected = "Some-CPlusPlusUrlCPlusPlus-With-UglyCSharpCSharp-Symbol";
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToUrlMethodShouldRemoveUglySymbolsAtBeginningAndEnd()
+        {
+            var original = "% Some  C++UrlC++  With  UglyC#C#  Symbol #";
+
+            var result = original.ToUrl();
+            var expected = "Some-CPlusPlusUrlCPlusPlus-With-UglyCSharpCSharp-Symbol";
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToUrlMethodShouldWorkWithCyrillicLetters()
+        {
+            var original = "Николай Костов";
+
+            var result = original.ToUrl();
+            var expected = "Николай-Костов";
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
--- MissingFeatures/StringExtensions.cs
+++ MissingFeatures/StringExtensions.cs
@@ -224,5 +224,30 @@
         {
             return Uri.UnescapeDataString(input);
         }
+
+        public static string ToUrl(this string uglyString)
+        {
+            var resultString = new StringBuilder(uglyString.Length);
+            bool isLastCharacterDash = false;
+
+            uglyString = uglyString.Replace("C#", "CSharp");
+            uglyString = uglyString.Replace("C++", "CPlusPlus");
+
+            foreach (var character in uglyString)
+            {
+                if (char.IsLetterOrDigit(character))
+                {
+                    resultString.Append(character);
+                    isLastCharacterDash = false;
+                }
+                else if (!isLastCharacterDash)
+                {
+                    resultString.Append('-');
+                    isLastCharacterDash = true;
+                }
+            }
+
+            return resultString.ToString().Trim('-');
+        }
     }
 }
