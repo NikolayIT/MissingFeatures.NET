5c3cb9eca6ffb9ffd017f36e3efcc9377baa2d79
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-09-11 22:27:19 +0300

    Implemented CreateDelegate MethodInfo extension.

---
+++ MissingFeatures/MethodInfoExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace MissingFeatures
+{
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Reflection;
+
+    public static class MethodInfoExtensions
+    {
+        public static Delegate CreateDelegate(this MethodInfo methodInfo, object target = null)
+        {
+            if (methodInfo == null)
+            {
+                throw new ArgumentNullException(nameof(methodInfo));
+            }
+
+            var delegateTypeArgs = methodInfo
+                .GetParameters()
+                .Select(p => p.ParameterType)
+                .Concat(new[] { methodInfo.ReturnType })
+                .ToArray();
+
+            var delegateType = Expression.GetDelegateType(delegateTypeArgs);
+
+            var @delegate = Delegate.CreateDelegate(delegateType, target, methodInfo);
+            return @delegate;
+        }
+    }
+}
