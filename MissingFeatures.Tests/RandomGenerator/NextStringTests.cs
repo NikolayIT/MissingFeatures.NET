e4c50883d1ef48bbc40107f5943b45bab3d87833
Nikolay Kostov <admin@nikolay.it>
2015-09-29 16:31:32 +0300

    Added unit test for NextString method in RandomGenerator

---
+++ MissingFeatures.Tests/RandomGenerator/NextStringTests.cs
@@ -0,0 +1,19 @@
+﻿namespace MissingFeatures.Tests.RandomGenerator
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using RandomGenerator = MissingFeatures.RandomGenerator;
+
+    [TestClass]
+    public class NextStringTests
+    {
+        [TestMethod]
+        public void NextStringShouldReturnStringWithSameNumberOfCharactersAsMethodParameter()
+        {
+            const int NumberOfCharacters = 7;
+            var randomGenerator = new RandomGenerator();
+            var result = randomGenerator.NextString(NumberOfCharacters);
+            Assert.AreEqual(NumberOfCharacters, result.Length);
+        }
+    }
+}
