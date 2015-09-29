570ff053a9ab176afdcf313a1a91f5c37809572e
Nikolay Kostov <admin@nikolay.it>
2015-09-29 15:32:48 +0300

    Added static method Code.ExecuteWithTimeLimit (with unit tests)

---
+++ MissingFeatures.Tests/Code/ExecuteWithTimeLimitTests.cs
@@ -0,0 +1,40 @@
+﻿namespace MissingFeatures.Tests.Code
+{
+    using System;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Code = MissingFeatures.Code;
+
+    [TestClass]
+    public class ExecuteWithTimeLimitTests
+    {
+        [TestMethod]
+        public void EmptyActionShouldReturnTrue()
+        {
+            var result = Code.ExecuteWithTimeLimit(
+                TimeSpan.FromSeconds(1),
+                () =>
+                    {
+                    });
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        [Timeout(2000)]
+        public void TestEndlessLoopShouldNotBlockAndReturnFalse()
+        {
+            var result = Code.ExecuteWithTimeLimit(
+                TimeSpan.FromSeconds(1),
+                () =>
+                {
+                    while (true)
+                    {
+                    }
+                });
+
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ MissingFeatures/Code.cs
@@ -0,0 +1,23 @@
+﻿namespace MissingFeatures
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public static class Code
+    {
+        // http://stackoverflow.com/questions/7413612/how-to-limit-the-execution-time-of-a-function-in-c-sharp/10544579#10544579
+        public static bool ExecuteWithTimeLimit(TimeSpan timeSpan, Action codeBlock)
+        {
+            try
+            {
+                var task = Task.Factory.StartNew(codeBlock);
+                task.Wait(timeSpan);
+                return task.IsCompleted;
+            }
+            catch (AggregateException ae)
+            {
+                throw ae.InnerExceptions[0];
+            }
+        }
+    }
+}
